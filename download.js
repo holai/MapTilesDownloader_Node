a2b9023ce33a1f855ecaa786e4308bbeafb54533
路昭文 <1270123648@qq.com>
2022-09-28 19:03:43 +0800

    save

--- download.js
+++ download.js
@@ -38,11 +38,44 @@ app.use((req, res, next) => {
 app.use(express.static("UI"));
 
 app.use("/start-download", function (req, res) {
-  console.log(req.fields);
+  let fields = req.fields;
+  let pathCreate = path.join(__dirname, "/out/" + fields.timestamp + "/");
+  try {
+    fs.accessSync(pathCreate);
+    res.status(500).json({ error: "此时间已失效" });
+    return;
+  } catch (error) {
+    fs.mkdirSync(pathCreate);
+  }
+  // console.log(req.fields);
+  // {
+  //   minZoom: '1',
+  //   maxZoom: '2',
+  //   outputDirectory: '{timestamp}',
+  //   outputFile: '{z}/{x}/{y}.png',
+  //   outputType: 'directory',
+  //   outputScale: '1',
+  //   source: 'http://ecn.t0.tiles.virtualearth.net/tiles/a{quad}.jpeg?g=129&mkt=en&stl=H',
+  //   timestamp: '1664360729877',
+  //   bounds: '-73.99017513233417,40.73779215352124,-73.93369853931902,40.76458087245638',
+  //   center: '-73.9619368358266,40.75118651298881,2'
+  // }
   res.json({ code: 200 });
 });
 app.use("/download-tile", function (req, res) {
   console.log(req.fields);
+  // x: 0
+  // y: 0
+  // z: 1
+  // quad: 0
+  // outputDirectory: {timestamp}
+  // outputFile: {z}/{x}/{y}.png
+  // outputType: directory
+  // outputScale: 1
+  // timestamp: 1664360433718
+  // source: http://ecn.t0.tiles.virtualearth.net/tiles/a{quad}.jpeg?g=129&mkt=en&stl=H
+  // bounds: -73.99017513233417,40.73779215352124,-73.93369853931902,40.76458087245638
+  // center: -73.9619368358266,40.75118651298881,2
 });
 app.listen(9100, () => {
   console.log(`server is running on port ${9100}`);
